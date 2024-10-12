94bd5ccdbb17c00e30ed06fc8d406849d204dab3
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-10-12 19:16:05 -0400

    added submodules

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from src.jp_imports.src.jp_imports.data_process import DataProcess
+from src.jp_imports.src.jp_imports.data_process import DataProcess as DataTrade
+from src.jp_index.src.data.data_process import DataProcess as DataIndex
 from fastapi.responses import FileResponse
+from dotenv import load_dotenv
 from fastapi import FastAPI
 import os
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -9,16 +13,44 @@ app = FastAPI()
 def index():
     return {"message": "Hello World"}
 
-@app.get("/data/imports/")
+# Endpoint to get the DataFrames
+@app.get("/data/trade/")
 async def get_data(time:str, types:str, agr:bool=False, group:bool=False, update:bool=False):
-    dp = DataProcess("data/")
+    dp = DataTrade("data/")
     df = dp.process_int_jp(time, types, agr, group, update).collect()
     return df.to_dicts()
 
-@app.get("/files/imports/")
-async def get_file(time:str, types:str, agr:bool=False, group:bool=False, update:bool=False):
-    dp = DataProcess("data/")
+@app.get("/data/index/consumer")
+async def get_consumer(update:bool=False):
+    dp = DataIndex(str(os.environ.get("DATABASE_URL")))
+    return dp.consumer_data(update).to_dicts()
+
+@app.get("/data/index/jp_index")
+async def get_jp_index(update:bool=False):
+    dp = DataIndex(str(os.environ.get("DATABASE_URL")))
+    return dp.jp_index_data(update).to_dicts()
+
+# Endpoints to download files
+@app.get("/files/trade/")
+async def get_trade_file(time:str, types:str, agr:bool=False, group:bool=False, update:bool=False):
+    dp = DataTrade("data/")
     df = dp.process_int_jp(time, types, agr, group, update).collect()
     file_path = os.path.join(os.getcwd(), "data", f"{time}_{types}.csv")
     df.write_csv(file_path)
     return FileResponse(file_path, media_type='text/csv', filename=f"{time}_{types}.csv")
+
+@app.get("/files/index/consumer")
+async def get_consumer_file(update:bool=False):
+    dp = DataIndex(str(os.environ.get("DATABASE_URL")))
+    df = dp.consumer_data(update)
+    file_path = os.path.join(os.getcwd(), "data", "consumer.csv") #TODO: Change to temp file
+    df.write_csv(file_path)
+    return FileResponse(file_path, media_type='text/csv', filename="consumer.csv")
+
+@app.get("/files/index/jp_index")
+async def get_jp_index_file(update:bool=False):
+    dp = DataIndex(str(os.environ.get("DATABASE_URL")))
+    df = dp.jp_index_data(update)
+    file_path = os.path.join(os.getcwd(), "data", "jp_index.csv")
+    df.write_csv(file_path)
+    return FileResponse(file_path, media_type='text/csv', filename="jp_index.csv")
