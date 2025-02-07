d9305617fda3b8d889df667793627a0d55b9ab53
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-02-07 16:58:58 +0000

    14 improve api (#24)
    * Update the "/data/trade/jp/" endpoint
    
    * bump version to 0.2.0 in pyproject.toml
    
    * update submodule to latest commit
    
    * Add new endpoint for HTS codes and update existing data processing function
    
    ---------
    
    Co-authored-by: Fernando2908 <fernando.sierra3@upr.edu>

--- env.py
+++ env.py
@@ -3,7 +3,6 @@ import os
 
 load_dotenv()
 
-
 def read_secret_file(secret_path):
     with open(secret_path, "r") as file:
         return file.read().strip()
@@ -15,10 +14,12 @@ def db_credentials():
     PORT = str(os.environ.get("POSTGRES_PORT")).strip()
     if os.environ.get("DEV") == "True":
         DB_PASSWORD = str(os.environ.get("POSTGRES_PASSWORD")).strip()
+        PORT = str(os.environ.get("POSTGRES_PORT")).strip()
         HOST = "localhost"
         DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{HOST}:{PORT}/{DB_DB}"
     else:
         DB_PASSWORD = str(read_secret_file("/run/secrets/db-password")).strip()
         HOST = "database"
-        DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{HOST}:5432/{DB_DB}"
+    
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{HOST}:{PORT}/{DB_DB}"
     return DATABASE_URL
