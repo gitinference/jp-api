799beadb93dadcb4c1513d3d2c86beae4ed3e1bf
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-02-24 16:09:12 +0000

    added support for coorp (#37)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-
 from .routers import data, files, graphs
 
 app = FastAPI()
 
+origins = ["http://localhost:3000", "http://192.168.50.24:5751"]
+
 app.include_router(data.router)
 app.include_router(files.router)
 app.include_router(graphs.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
