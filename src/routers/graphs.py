d5202329f5475f9caedd9df2688a5a25c8cc70dc
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-06-19 12:41:05 +0000

    bump submoduels and change paths for db (#84)

--- src/routers/graphs.py
+++ src/routers/graphs.py
@@ -13,6 +13,7 @@ router = APIRouter()
 #     graph = graphGenerator().create_graph(naics_code)
 #     return graph.to_json()
 
+
 @router.get("/graph/import-export/")
 async def get_imports_exports_graph(
     level: str,
@@ -27,32 +28,33 @@ async def get_imports_exports_graph(
     type: str = "",
 ):
     if type == "imports":
-        graph = DataGraph().gen_imports_chart(
-            level = level,
-            time_frame = time_frame,
-            agriculture_filter = agriculture_filter,
-            group = group,
-            level_filter = level_filter,
-            datetime = datetime,
+        graph = DataGraph(database_file="data/data.ddb").gen_imports_chart(
+            level=level,
+            time_frame=time_frame,
+            agriculture_filter=agriculture_filter,
+            group=group,
+            level_filter=level_filter,
+            datetime=datetime,
             frequency=frequency,
-            second_dropdown = second_dropdown,
-            third_dropdown = third_dropdown,
+            second_dropdown=second_dropdown,
+            third_dropdown=third_dropdown,
         )
     elif type == "exports":
-        graph = DataGraph().gen_exports_chart(
-            level = level,
-            time_frame = time_frame,
-            agriculture_filter = agriculture_filter,
-            group = group,
-            level_filter = level_filter,
-            datetime = datetime,
+        graph = DataGraph(database_file="data/data.ddb").gen_exports_chart(
+            level=level,
+            time_frame=time_frame,
+            agriculture_filter=agriculture_filter,
+            group=group,
+            level_filter=level_filter,
+            datetime=datetime,
             frequency=frequency,
-            second_dropdown = second_dropdown,
-            third_dropdown = third_dropdown,
+            second_dropdown=second_dropdown,
+            third_dropdown=third_dropdown,
         )
     else:
         raise ValueError("Invalid type specified. Use 'imports' or 'exports'.")
     return graph.to_html(fullhtml=False, output_div=type)
+
 
 @router.get("/graph/product-hts/")
 async def get_product_hts_graph(
@@ -63,84 +65,94 @@ async def get_product_hts_graph(
     time_frame: str = "",
     trade_type: str = "",
 ):
-    graph, context = DataGraph().gen_hts_chart(
+    graph, context = DataGraph(database_file="data/data.ddb").gen_hts_chart(
         level=level,
         agriculture_filter=agriculture_filter,
         group=group,
         level_filter=level_filter,
         frequency=time_frame,
-        trade_type=trade_type
+        trade_type=trade_type,
     )
     return graph.to_html(), context
 
+
 @router.get("/graph/product-ranking/")
 async def get_hts_ranking_graph():
-    graphs = DataGraph().gen_hts_ranking_chart()
+    graphs = DataGraph(database_file="data/data.ddb").gen_hts_ranking_chart()
     return {
-            "export_top": graphs['export_top'].to_html(fullhtml=False, output_div='export_top'),
-            "export_bottom": graphs['export_bottom'].to_html(fullhtml=False, output_div='export_bottom'),
-            "import_top": graphs['import_top'].to_html(fullhtml=False, output_div='import_top'),
-            "import_bottom": graphs['import_bottom'].to_html(fullhtml=False, output_div='import_bottom'),
-        }
+        "export_top": graphs["export_top"].to_html(
+            fullhtml=False, output_div="export_top"
+        ),
+        "export_bottom": graphs["export_bottom"].to_html(
+            fullhtml=False, output_div="export_bottom"
+        ),
+        "import_top": graphs["import_top"].to_html(
+            fullhtml=False, output_div="import_top"
+        ),
+        "import_bottom": graphs["import_bottom"].to_html(
+            fullhtml=False, output_div="import_bottom"
+        ),
+    }
+
 
 @router.get("/graph/naics/")
 async def get_naics_graph(
     naics_code: str,
 ):
-    graph, context = graphGenerator().gen_naics_graph(
+    graph, context = graphGenerator(database_file="data/data.ddb").gen_naics_graph(
         naics_code=naics_code,
     )
     return graph.to_html(), context
+
 
 @router.get("/graph/indicadores/")
 async def get_indicadores_graph(
     time_frame: str,
     column: str,
 ):
-    graph, columns = IndexDataGraph().create_indicators_graph(
+    graph, columns = IndexDataGraph(
+        database_file="data/data.ddb"
+    ).create_indicators_graph(
         time_frame=time_frame,
         column=column,
     )
 
     context = {
-        'columns': columns,
+        "columns": columns,
     }
 
     return graph.to_html(), context
 
+
 @router.get("/graph/consumer/")
-async def get_consumer_graph(
-    time_frame: str,
-    column: str,
-    data_type: str
-):
-    graph, columns = IndexDataGraph().create_consumer_graph(
-        time_frame=time_frame,
-        column=column,
-        data_type=data_type
-    )
+async def get_consumer_graph(time_frame: str, column: str, data_type: str):
+    graph, columns = IndexDataGraph(
+        database_file="data/data.ddb"
+    ).create_consumer_graph(time_frame=time_frame, column=column, data_type=data_type)
 
     context = {
-        'columns': columns,
+        "columns": columns,
     }
 
-    return graph.to_html(fullhtml=False, output_div=f'{data_type}_indices_consumidor'), context
+    return graph.to_html(
+        fullhtml=False, output_div=f"{data_type}_indices_consumidor"
+    ), context
+
 
 @router.get("/graph/energia/")
 async def get_energy_graph(
-    period: str = "monthly",
-    metric: str = "generacion_neta_mkwh"
+    period: str = "monthly", metric: str = "generacion_neta_mkwh"
 ):
-    graph, energy_metrics = IndexDataGraph().create_energy_chart(
-        period=period,
-        metric=metric
-    )
+    graph, energy_metrics = IndexDataGraph(
+        database_file="data/data.ddb"
+    ).create_energy_chart(period=period, metric=metric)
 
     context = {
-        'energy_metrics': energy_metrics,
+        "energy_metrics": energy_metrics,
     }
 
-    return graph.to_html(fullhtml=False, output_div='energy'), context
+    return graph.to_html(fullhtml=False, output_div="energy"), context
+
 
 @router.get("/graph/awards/category")
 async def get_awards_category_graph(
@@ -149,35 +161,39 @@ async def get_awards_category_graph(
     third_dropdown: str,
     time_frame: str,
 ):
-    graph, categories = IndexDataGraph().create_spending_by_category_graph(
+    graph, categories = IndexDataGraph(
+        database_file="data/data.ddb"
+    ).create_spending_by_category_graph(
         year=dropdown,
         quarter=second_dropdown,
         month=second_dropdown,
         type=time_frame,
-        category=third_dropdown
+        category=third_dropdown,
     )
 
     context = {
-        'categories': categories,
+        "categories": categories,
     }
 
-    return graph.to_html(fullhtml=False, output_div='category'), context
+    return graph.to_html(fullhtml=False, output_div="category"), context
+
 
 @router.get("/graph/awards/secter")
 async def get_awards_secter_graph(
     dropdown: str,
     time_frame: str,
 ):
-    graph, agencies = IndexDataGraph().create_secter_graph(
+    graph, agencies = IndexDataGraph(database_file="data/data.ddb").create_secter_graph(
         type=time_frame,
         secter=dropdown,
     )
 
     context = {
-        'agencies': agencies,
+        "agencies": agencies,
     }
 
-    return graph.to_html(fullhtml=False, output_div='secter'), context
+    return graph.to_html(fullhtml=False, output_div="secter"), context
+
 
 @router.get("/graph/indices/precios")
 async def get_indices_precios_graph(
@@ -185,14 +201,14 @@ async def get_indices_precios_graph(
     data_type: str,
     column: str,
 ):
-    graph, columns = IndexDataGraph().create_price_index_graph(
-        time_frame=time_frame,
-        data_type=data_type,
-        column=column
+    graph, columns = IndexDataGraph(
+        database_file="data/data.ddb"
+    ).create_price_index_graph(
+        time_frame=time_frame, data_type=data_type, column=column
     )
 
     context = {
-        'columns_2': columns,
+        "columns_2": columns,
     }
 
-    return graph.to_html(fullhtml=False, output_div='indices_precios'), context
+    return graph.to_html(fullhtml=False, output_div="indices_precios"), context
